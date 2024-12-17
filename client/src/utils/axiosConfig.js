615c0db074b5988a367dca53aefd6a6f5787f1ba
Mhark Anthony <82895726+ynodev@users.noreply.github.com>
2024-12-18 02:24:16 +0800

    Update axiosConfig.js

--- client/src/utils/axiosConfig.js
+++ client/src/utils/axiosConfig.js
@@ -1,28 +1,34 @@
 import axios from 'axios';
 
-axios.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem('token');
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-        config.withCredentials = true;
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
+const axiosInstance = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  withCredentials: true,
+});
+
+// Request interceptor
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
 );
 
-axios.interceptors.response.use(
-    (response) => response,
-    (error) => {
-        if (error.response?.status === 401) {
-            localStorage.removeItem('token');
-            window.location.href = '/login';
-        }
-        return Promise.reject(error);
+// Response interceptor
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem('token');
+      window.location.href = '/login';
     }
+    return Promise.reject(error);
+  }
 );
 
-export default axios; 
+export default axiosInstance; 
